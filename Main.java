470e0a7b70ae8f179f24c80c817b993b156015d1
AP CODEZ <104022036+APCODEZ@users.noreply.github.com>
2022-10-25 17:40:28 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+    int num = 3;
+    num += 3; //This is the same as --> num = num + 3;
+      System.out.println(num);
+    num -= 3;
+      System.out.println(num);
+    num ++; /*This gives the most recent value of num and     
+    adds it with 1*/
+      System.out.println(num);
+  }
+}
